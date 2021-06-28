b1ad17b6033cb5bc3633aa10ff7134b05187caee
okorkov <alex.okorkov@gmail.com>
2021-06-28 15:41:20 -0700

    updated index.html with approprate meta and package json

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+  
+  const BASE_URL = process.env.BASE_URL
+
   return (
     <div className="App">
-      <header className="App-header">
-       <h1>Website is under constraction, check back soon.</h1>
-      </header>
+      
     </div>
   );
 }
