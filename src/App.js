359dc49e1c82178d0f2105598b73b14c715e09d9
okorkov <alex.okorkov@gmail.com>
2021-06-28 16:02:30 -0700

    added switch and routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import ErrorPage from './components/ErrorPage'
+import Dashboard from './components/Dashboard'
 
 function App() {
 
   const BASE_URL = process.env.BASE_URL
 
   return (
-    <div className="App">
-      Welcome
-    </div>
+    <Router>
+      <Switch>
+        <Route path={'/dashboard'} exact component={Dashboard} />
+        <Route component={ErrorPage} />
+      </Switch>
+    </Router>
   );
 }
 
