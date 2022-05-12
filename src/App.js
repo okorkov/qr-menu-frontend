5d68c930f9628c3ac3e963c8b1b6f211d5b72a62
okorkov <alex.okorkov@gmail.com>
2022-05-12 10:09:30 -0700

    deprecating project

--- src/App.js
+++ src/App.js
@@ -16,9 +16,7 @@ import MenuComponent from "./components/MenuComponent";
 import Qrlinks from "./components/Qrlinks";
 import Deprecationmodal from "./components/DeprecationModal";
 
-
-
-
+const IS_ACTIVE_PROJECT = false;
 
 function App(props) {
 
@@ -41,23 +39,33 @@ function App(props) {
   }, []);
 
   return (
-    <Router>
-      <Navbar />
-      <Switch>
-        <Route path={'/'} exact component={Home} />
-        <Route path={'/dashboard'} exact component={Dashboard} />
-        <Route path={'/contact'} exact component={Contact} />
-        <Route path={'/about'} exact component={About} />
-        <Route path={'/demo'} exact component={Demo} />
-        <Route path={'/single-file'} exact component={SingleFileComponent} />
-        <Route path={'/qr-menu'} exact component={MenuComponent} />
-        <Route path={'/qr-link'} exact component={Qrlinks} />
-        <Route path={'/menu/:id'} component={RenderMenu} />
-        <Route component={ErrorPage} />
-      </Switch>
-      <Deprecationmodal />
-      <Footer />
-    </Router>
+    <>
+    {
+      IS_ACTIVE_PROJECT ?
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path={'/'} exact component={Home} />
+          <Route path={'/dashboard'} exact component={Dashboard} />
+          <Route path={'/contact'} exact component={Contact} />
+          <Route path={'/about'} exact component={About} />
+          <Route path={'/demo'} exact component={Demo} />
+          <Route path={'/single-file'} exact component={SingleFileComponent} />
+          <Route path={'/qr-menu'} exact component={MenuComponent} />
+          <Route path={'/qr-link'} exact component={Qrlinks} />
+          <Route path={'/menu/:id'} component={RenderMenu} />
+          <Route component={ErrorPage} />
+        </Switch>
+        <Deprecationmodal />
+        <Footer />
+      </Router>
+      :
+      <div style={{textAlign: 'center', height: '100vh', paddingTop:'15%'}}>
+        <p className='text-title'>Project has been deprecated</p>
+      </div>
+    }
+   
+    </>
   );
 }
 
